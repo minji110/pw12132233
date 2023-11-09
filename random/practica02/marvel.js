4664fe47722e6419d29418ca9e6b3c9b602c877a
minji110 <airamguadalupe702@gmail.com>
2023-11-08 22:39:08 -0700

    practica marvel ya quedo tu

--- random/practica02/marvel.js
+++ random/practica02/marvel.js
@@ -1,40 +1,46 @@
-var numero=0;
 var cadena="";
+var numero=0;
 var datos=[];
 
-async function apirandommovie(){
-    cadena=document.getElementById("pelicula");
+async function apibuscarheroe(){
+    cadena = document.getElementById("heroe").value;
     const respuesta = await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="+cadena);
-    //"https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="+cadena
     //https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c
     datos = await respuesta.json();
-
     muestra();
 }
 
 function muestra(){
-    let foto=document.getElementById("foto");
+    console.log();
     let nombre=document.getElementById("nombre");
     let descripcion=document.getElementById("descripcion");
+    let foto=document.getElementById("foto");
+    let comic=document.getElementById("comics");
 
-    foto.setAttribute("src", datos.results[0].thumbnail);
-    nombre.innerHTML=datos.results[0].name;
-    descripcion.innerHTML=datos.results[0].description;
+    //siempre poner el data porque si no, no jala esto y duraras 2 hora buscando el error ;_;
+    foto.setAttribute("src", datos.data.results[numero].thumbnail.path + "." + datos.data.results[numero].thumbnail.extension);
+    nombre.innerHTML = datos.data.results[numero].name;
+    descripcion.innerHTML = datos.data.results[numero].description;
+    comic.innerHTML=datos.data.results[numero].comics.available;
 }
 
-//esto ya esta
-function anterior(){
-    if (numero>0){
-        muestra();
-        numero--;
+function anterior() {
+    // Validar si numero es menor que 0
+    numero--;
+    if (numero < 0) {
+        numero = datos.data.results.length - 1;
     }
     muestra();
 }
-function siguiente(){
-    // validar que no se pase
-    if (numero <datos.results.leght-1){
-        numero++;
-       muestra();
+
+function siguiente() {
+    // Validar si numero se pasa del límite
+    numero++;
+    if (numero === datos.data.results.length) {
+        numero = 0;
     }
-    
+    muestra();
 }
+
+
+
